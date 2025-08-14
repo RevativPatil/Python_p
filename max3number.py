56552d1a38aacdafb6790244c9aad132dd2c4b30
Vrushali Patil <vrushalipatil@Vrushalis-MacBook-Air.local>
2025-08-14 21:20:25 +0530

    added files

---
+++ max3number.py
@@ -0,0 +1,12 @@
+N1=int(input("enter the first number\n"))
+N2=int(input("Enter second number\n"))
+N3=int(input("Enter third number\n"))
+
+if(N1>N2 & N1>N3):
+    print(N1,"is grater")
+elif(N2>N1 & N2>N3):
+    print(N2,"is grater")
+elif(N3>N1 & N3>N2):
+    print(N3,"is grater")
+
+    
