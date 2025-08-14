67c4c1c6f9e47d1894221d692287499e7c439b17
Vrushali Patil <vrushalipatil@Vrushalis-MacBook-Air.local>
2025-08-14 21:27:05 +0530

    added vowel.py file

---
+++ vowel.py
@@ -0,0 +1,6 @@
+chr = input("Enter a character:\n")
+
+if(chr=='a' or chr=='e' or chr=='i' or chr=='o' or chr=='u' or chr=='A' or chr=='E' or chr=='I' or chr=='O' or chr=='U' ):
+    print(chr, "is a Vowel")
+else:
+    print(chr, "is not a Vowel")
